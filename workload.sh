ef8d3078cab6eddaf78c71a597f3f5861ade09c8
Starofall <starofall@gmail.com>
2018-10-09 03:47:25 +0200

    workload

--- workload.sh
+++ workload.sh
@@ -1,1 +1,4 @@
 python start.py start scenarios/a-singleRun/5-smartForest
+python start.py start scenarios/a-singleRun/6-smartGauss
+python start.py start scenarios/a-singleRun/91-forest
+python start.py start scenarios/a-singleRun/92-boostedTrees
