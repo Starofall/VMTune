73d6b8c6805f454ebca821504b14e3e95afcbdd3
Heaven Test Maschine <heaven@testmachine.nope>
2018-10-10 13:29:55 +0200

    Executed Tests on Target Machine

--- workload.sh
+++ workload.sh
@@ -1,4 +1,2 @@
-python start.py start scenarios/a-singleRun/5-smartForest
-python start.py start scenarios/a-singleRun/6-smartGauss
 python start.py start scenarios/a-singleRun/91-forest
 python start.py start scenarios/a-singleRun/92-boostedTrees
