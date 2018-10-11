c3d87dadbd3b9fd2e21a39669338f484e582da9c
Starofall <starofall@gmail.com>
2018-10-11 23:33:25 +0200

    folder renaming / human optimize of b

--- workload.sh
+++ workload.sh
@@ -1,4 +1,1 @@
-python start.py start scenarios/a-singleRun/7-smartForestBackground
-python start.py start scenarios/b-fastest/0-baseline
-python start.py start scenarios/c-memoryLimited/0-baseline
-python start.py start scenarios/d-lowVariance/0-baseline
+python start.py start scenarios/b-fastest/1-humanForest
