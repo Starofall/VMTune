d8360862d680181c793f7e9a1543a2098642128b
Starofall <starofall@gmail.com>
2018-10-11 02:34:37 +0200

    workload

--- workload.sh
+++ workload.sh
@@ -1,1 +1,4 @@
-python start.py start scenarios/a-singleRun/5-smartForest
+python start.py start scenarios/a-singleRun/7-smartForestBackground
+python start.py start scenarios/b-fastest/0-baseline
+python start.py start scenarios/c-memoryLimited/0-baseline
+python start.py start scenarios/d-lowVariance/0-baseline
